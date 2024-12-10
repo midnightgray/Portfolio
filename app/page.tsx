c3cdf8cd25c3643dff4d7966b3a39b30892e791a
Madeleine <144938149+midnightgray@users.noreply.github.com>
2024-12-10 13:21:39 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          4th Year Computer Science Student at Adamson University.
+          4th Year Computer Science Student at Adamson University
         </h2>
       </div>
     </div>
